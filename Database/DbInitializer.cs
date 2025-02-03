a4373b88f7fceeb032629f10e99bd5b902380e1d
Amr <Amr.Mostafa@live.com>
2025-02-03 12:59:38 +1100

    don't alter db

--- Database/DbInitializer.cs
+++ Database/DbInitializer.cs
@@ -5,14 +5,15 @@ public static class DbInitializer
 {
     public static void Initialize(AvanciraDbContext context, UserManager<User> userManager)
     {
-        context.Database.EnsureDeleted();
+        // context.Database.EnsureDeleted();
 
-        context.Database.EnsureCreated();
+        // context.Database.EnsureCreated();
 
-        RoleSeeder.Seed(context, userManager);
-        UserSeeder.Seed(context, userManager);
-        LessonCategorySeeder.Seed(context, userManager);
-        ListingSeeder.Seed(context, userManager);
+        // RoleSeeder.Seed(context, userManager);
+        // UserSeeder.Seed(context, userManager);
+        // LessonCategorySeeder.Seed(context, userManager);
+        // ListingSeeder.Seed(context, userManager);
+
         // LessonSeeder.Seed(context);
         // ReviewSeeder.Seed(context);
         // ChatSeeder.Seed(context);
